7b94cf3c6804175fcf86b4524841e4e554174205
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-16 01:58:06 -0500

    Añadiendo y configurando la carpeta vendor

--- includes/app.php
+++ includes/app.php
@@ -1,3 +1,7 @@
 <?php 
+require 'funciones.php';
+require 'config/database.php';
+require __DIR__.'/../vendor/autoload.php';
+
 define('TEMPLATES_URL', __DIR__.'/templates');
 define('FUNCIONES_URL', __DIR__.'funciones.php');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b36f2d17ba3fb9edc95bef48a2cbca5900c3a9c4',
+    'name' => 'richa/bienesraices_inicio',
+  ),
+  'versions' => 
+  array (
+    'richa/bienesraices_inicio' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b36f2d17ba3fb9edc95bef48a2cbca5900c3a9c4',
+    ),
+  ),
+);
