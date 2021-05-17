da4726122dc1c6ef9dcf73ac6feea8de0c6bf384
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-16 22:42:16 -0500

    Sanitizando atributos en la clase propiedad

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -4,7 +4,8 @@ namespace App;
 class Propiedad{
     //base de datos
     protected static $db;
-
+    protected static $columnaBD = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
+    
     public $id;
     public $titulo;
     public $precio;
@@ -15,6 +16,11 @@ class Propiedad{
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+
+    //Definir la conexión a la BD
+    public static function setDB($database){
+        self::$db = $database;
+    }
 
     public function __construct($args = [])
     {
@@ -32,6 +38,9 @@ class Propiedad{
 
     public function guardar()
     {
+        //Sanitizar los datos
+        $atributos = $this->sanitizarAtributos();
+
         //Insertar en la base de datos
         $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) 
         VALUES('$this->titulo','$this->precio', '$this->imagen', '$this->descripcion','$this->habitaciones','$this->wc','$this->estacionamiento', '$this->creado','$this->vendedorId')";
@@ -40,8 +49,22 @@ class Propiedad{
         debuguear($resultado);
     }
 
-    //Definir la conexión a la BD
-    public static function setDB($database){
-        self::$db = $database;
+    //Identificar y unir los atributos de la BD
+    public function atributos(){
+        $atributos = [];
+        foreach (self::$columnaBD as $columna) {
+            if ($columna === 'id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
     }
+
+    public function sanitizarAtributos(){
+        $atributos =  $this->atributos();
+        $sanitizado = [];
+        foreach ($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+        return $sanitizado;
+    }    
 }
