f8bb1604e8b8c3d6ea7f7ce4d439e8dae26a4c25
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-18 03:49:03 -0500

    Agregando formulario_propiedad

---
+++ includes/templates/formulario_propiedades.php
@@ -0,0 +1,32 @@
+<fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo sane( $propiedad->titulo ); ?>" >
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo sane( $propiedad->precio ); ?>">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg image/png" name="imagen">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea name="descripcion" id="descripcion" cols="30" rows="10"><?php echo sane($propiedad->descripcion);?></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de la Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo sane($propiedad->habitaciones); ?>">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo sane($propiedad->wc); ?>">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo sane($propiedad->estacionamiento); ?>">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+            </fieldset>
